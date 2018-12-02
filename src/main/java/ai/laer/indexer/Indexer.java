ef6d45675a3a90fdcbc2ff0aac4dac9f4e962a2b
nishant_home <nishant.rathore12@gmail.com>
2018-12-02 08:26:16 +0530

    changes to store field

--- src/main/java/ai/laer/indexer/Indexer.java
+++ src/main/java/ai/laer/indexer/Indexer.java
@@ -1,5 +1,6 @@
 package ai.laer.indexer;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
@@ -11,58 +12,55 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
 public class Indexer {
-    private IndexWriter writer;
-    private String uriPrefix = "http://laer.ai/enron/email/id/";
+	private IndexWriter writer;
+	private String uriPrefix = "http://laer.ai/enron/email/id/";
 
-   public Indexer(String indexDirectoryPath) throws IOException {
-      //this directory will contain the indexes
+	public Indexer(String indexDirectoryPath) throws IOException {
+		// this directory will contain the indexes
 
-       //Directory index = new RAMDirectory();
-      Directory indexDirectory = FSDirectory.open(Paths.get(indexDirectoryPath));
+		// Directory index = new RAMDirectory();
+		Directory indexDirectory = FSDirectory.open(Paths.get(indexDirectoryPath));
 
-       //create the indexer
-       writer = new IndexWriter(indexDirectory, new IndexWriterConfig());
+		// create the indexer
+		writer = new IndexWriter(indexDirectory, new IndexWriterConfig());
+	}
+
+	public void close() throws CorruptIndexException, IOException {
+		writer.close();
+	}
+
+	private Document getDocument(File file) throws IOException {
+	   
+	   try (InputStream stream = new FileInputStream(file))
+       {
+		   Document document = new Document();
+		   document.add(new TextField("contents", IOUtils.toString(stream, StandardCharsets.UTF_8.name()), 
+				   Field.Store.YES));
+		   document.add(new StringField("uri", uriPrefix + file.getName(), Field.Store.YES));
+
+		   return document;
+       }
    }
 
-   public void close() throws CorruptIndexException, IOException {
-      writer.close();
-   }
+	private void indexFile(File file) throws IOException {
+		Document document = getDocument(file);
+		writer.addDocument(document);
+	}
 
-   private Document getDocument(File file) throws IOException {
-      Document document = new Document();
+	public int createIndex(String dataDirPath, FileFilter filter) throws IOException {
+		// get all files in the data directory
+		File[] files = new File(dataDirPath).listFiles();
 
-      //document.add(contentField);
-       document.add(new TextField(LuceneConstants.CONTENTS, new FileReader(file)));
-      document.add(new StringField("uri", uriPrefix + file.getName(), Field.Store.YES));
-      //document.add(filePathField);
-
-      return document;
-   }
-
-   private void indexFile(File file) throws IOException {
-      Document document = getDocument(file);
-      writer.addDocument(document);
-   }
-
-   public int createIndex(String dataDirPath, FileFilter filter)
-      throws IOException {
-      //get all files in the data directory
-      File[] files = new File(dataDirPath).listFiles();
-
-      for (File file : files) {
-         if(!file.isDirectory()
-            && !file.isHidden()
-            && file.exists()
-            && file.canRead()
-            && filter.accept(file)
-         ){
-            indexFile(file);
-         }
-      }
-      return writer.numDocs();
-   }
+		for (File file : files) {
+			if (!file.isDirectory() && !file.isHidden() && file.exists() && file.canRead() && filter.accept(file)) {
+				indexFile(file);
+			}
+		}
+		return writer.numDocs();
+	}
 }
--- src/main/java/ai/laer/indexer/SimpleSearch.java
+++ src/main/java/ai/laer/indexer/SimpleSearch.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.core.SimpleAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -14,6 +15,13 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.highlight.Formatter;
+import org.apache.lucene.search.highlight.Fragmenter;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
+import org.apache.lucene.search.highlight.SimpleSpanFragmenter;
+import org.apache.lucene.search.highlight.TokenSources;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
@@ -34,13 +42,39 @@ public class SimpleSearch {
 		TopDocs docs = searcher.search(q, hitsPerPage);
 		ScoreDoc[] hits = docs.scoreDocs;
 		
+		//Uses HTML &lt;B&gt;&lt;/B&gt; tag to highlight the searched terms
+        Formatter formatter = new SimpleHTMLFormatter();
+         
+        //It scores text fragments by the number of unique query terms found
+        //Basically the matching score in layman terms
+        QueryScorer scorer = new QueryScorer(q);
+        
+		//used to markup highlighted terms found in the best sections of a text
+        Highlighter highlighter = new Highlighter(formatter, scorer);
+         
+        //It breaks text up into same-size texts but does not split up spans
+        Fragmenter fragmenter = new SimpleSpanFragmenter(scorer, 10);
 		
 		 // 4. display results
         System.out.println("Found " + hits.length + " hits.");
         for(int i=0;i<hits.length;++i) {
             int docId = hits[i].doc;
-            Document d = searcher.doc(docId);
-            System.out.println((i + 1) + ". "+"\t" + d.get("uri"));
+            Document doc = searcher.doc(docId);
+            System.out.println((i + 1) + ". "+"\t" + doc.get("uri"));
+            
+            //Get stored text from found document
+            String text = doc.get("contents");
+ 
+            //Create token stream
+            TokenStream stream = TokenSources.getAnyTokenStream(reader, docId, "contents", analyzer);
+             
+            //Get highlighted text fragments
+            String[] frags = highlighter.getBestFragments(stream, text, 10);
+            for (String frag : frags)
+            {
+                System.out.println("=======================");
+                System.out.println(frag);
+            }
         }
     }
 	
